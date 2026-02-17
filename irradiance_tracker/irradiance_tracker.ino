36770b3096ac4a95a5066ac10bf93c09b079fe48
Viyapakan Shanmugam <147246267+Viyapakan@users.noreply.github.com>
2026-02-17 23:21:23 +0530

    Feat_01:LED blink tested

--- irradiance_tracker/irradiance_tracker.ino
+++ irradiance_tracker/irradiance_tracker.ino
@@ -1,9 +1,21 @@
-void setup() {
-  // put your setup code here, to run once:
+// Most ESP32 DevKits have the on-board LED on GPIO 2
+const int ledPin = 2; 
 
+void setup() {
+  // Initialize the digital pin as an output
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
+  Serial.println("ESP32 Blink Challenge: Started!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Turn the LED on (HIGH is the voltage level)
+  digitalWrite(ledPin, HIGH);
+  Serial.println("LED is ON");
+  delay(1000); // Wait for a second
 
+  // Turn the LED off by making the voltage LOW
+  digitalWrite(ledPin, LOW);
+  Serial.println("LED is OFF");
+  delay(1000); // Wait for a second
 }
